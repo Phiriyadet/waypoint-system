4af1dd32a3239512e4bc085464a6077d036a3783
Phiriyadet <54772372+Phiriyadet@users.noreply.github.com>
2026-03-07 16:01:53 +0700

    feat(domain) : add new domain event

---
+++ backend/WayPoint/src/WayPoint.Domain/Events/DeliveryStartedEvent.cs
@@ -0,0 +1,12 @@
+﻿using WayPoint.Domain.Common;
+
+namespace WayPoint.Domain.Events;
+
+public record DeliveryStartedEvent(
+    Guid DeliveryId,
+    Guid RiderId
+) : IDomainEvent
+{
+    public Guid EventId => Guid.NewGuid();
+    public DateTime OccurredAt => DateTime.UtcNow;
+}
