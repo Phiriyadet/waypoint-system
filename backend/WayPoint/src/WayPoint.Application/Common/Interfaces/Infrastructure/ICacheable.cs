47b7565bf55465da5bfadebe9c5afae39d87120b
Phiriyadet <54772372+Phiriyadet@users.noreply.github.com>
2026-03-08 15:47:09 +0700

    feat(application) : add interfaces cach service

---
+++ backend/WayPoint/src/WayPoint.Application/Common/Interfaces/Infrastructure/ICacheable.cs
@@ -0,0 +1,8 @@
+﻿namespace WayPoint.Application.Common.Interfaces.Infrastructure;
+
+/// <summary>Implement interface นี้ใน Query ที่ต้องการ Redis cache</summary>
+public interface ICacheable
+{
+    string CacheKey { get; }
+    TimeSpan? Expiry => TimeSpan.FromMinutes(5);
+}
