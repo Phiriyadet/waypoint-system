df85e066abdb3d986aa644c3d3e088fe41c4708a
Phiriyadet <54772372+Phiriyadet@users.noreply.github.com>
2026-03-09 13:54:23 +0700

    feat(application) : add query Get Delivery

---
+++ backend/WayPoint/src/WayPoint.Application/Deliveries/Queries/GetDeliveries/GetDeliveriesQuery.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using WayPoint.Application.Deliveries.DTOs;
+using WayPoint.Domain.Common;
+using WayPoint.Domain.Enums;
+
+namespace WayPoint.Application.Deliveries.Queries.GetDeliveries;
+
+public record GetDeliveriesQuery(
+    Guid? RiderId,
+    DeliveryStatus? Status
+) : IRequest<Result<List<DeliveryDto>>>;
---
+++ backend/WayPoint/src/WayPoint.Application/Deliveries/Queries/GetDeliveries/GetDeliveriesQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using WayPoint.Application.Common.Interfaces.Repositories;
+using WayPoint.Application.Common.Mappings;
+using WayPoint.Application.Deliveries.DTOs;
+using WayPoint.Domain.Common;
+
+namespace WayPoint.Application.Deliveries.Queries.GetDeliveries;
+
+public class GetDeliveriesQueryHandler
+    : IRequestHandler<GetDeliveriesQuery, Result<List<DeliveryDto>>>
+{
+    private readonly IDeliveryRepository _repo;
+    private readonly DeliveryMapper _mapper;
+
+    public GetDeliveriesQueryHandler(IDeliveryRepository repo, DeliveryMapper mapper)
+    {
+        _repo = repo;
+        _mapper = mapper;
+    }
+
+    public async Task<Result<List<DeliveryDto>>> Handle(
+        GetDeliveriesQuery request, CancellationToken ct)
+    {
+        var deliveries = await _repo.GetAllAsync(request.RiderId, request.Status, ct);
+
+        var deliveryDtos = deliveries.Select(delivery => _mapper.ToDto(delivery)).ToList();
+
+        return Result<List<DeliveryDto>>.Success(deliveryDtos);
+    }
+}
