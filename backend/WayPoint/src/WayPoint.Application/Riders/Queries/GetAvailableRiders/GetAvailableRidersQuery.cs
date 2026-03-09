d1b8af6b94542f11f9891e41b1ba0738dcc22781
Phiriyadet <54772372+Phiriyadet@users.noreply.github.com>
2026-03-09 14:55:31 +0700

    feat(application) : add query Get Available Rider

---
+++ backend/WayPoint/src/WayPoint.Application/Riders/Queries/GetAvailableRiders/GetAvailableRidersQuery.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+using WayPoint.Application.Riders.DTOs;
+using WayPoint.Domain.Common;
+
+namespace WayPoint.Application.Riders.Queries.GetAvailableRiders;
+
+public record GetAvailableRidersQuery : IRequest<Result<List<RiderDto>>>;
---
+++ backend/WayPoint/src/WayPoint.Application/Riders/Queries/GetAvailableRiders/GetAvailableRidersQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using WayPoint.Application.Common.Interfaces.Repositories;
+using WayPoint.Application.Common.Mappings;
+using WayPoint.Application.Riders.DTOs;
+using WayPoint.Domain.Common;
+using WayPoint.Domain.Enums;
+
+namespace WayPoint.Application.Riders.Queries.GetAvailableRiders;
+
+public class GetAvailableRidersQueryHandler
+    : IRequestHandler<GetAvailableRidersQuery, Result<List<RiderDto>>>
+{
+    private readonly IRiderRepository _repo;
+    private readonly RiderMapper _mapper;
+
+    public GetAvailableRidersQueryHandler(IRiderRepository repo, RiderMapper mapper)
+    {
+        _repo = repo;
+        _mapper = mapper;
+    }
+
+    public async Task<Result<List<RiderDto>>> Handle(
+        GetAvailableRidersQuery request, CancellationToken ct)
+    {
+        var riders = await _repo.GetByStatusAsync(RiderStatus.Available, ct);
+
+        var riderDtos = riders.Select(rider => _mapper.ToDto(rider)).ToList();
+
+        return Result<List<RiderDto>>.Success(riderDtos);
+    }
+}
