d043c9dd3b2097bb77ad0661ee9dda2f11479f57
Phiriyadet <54772372+Phiriyadet@users.noreply.github.com>
2026-03-17 16:36:15 +0700

    refactor(application) : change GetAvailable to GetRider and refactor code in GetRiderQueryHandler

--- backend/WayPoint/src/WayPoint.Application/Riders/Queries/GetRiders/GetRidersQueryHandler.cs
+++ backend/WayPoint/src/WayPoint.Application/Riders/Queries/GetRiders/GetRidersQueryHandler.cs
@@ -1,28 +1,33 @@
-﻿using MediatR;
+using MediatR;
+
 using WayPoint.Application.Common.Interfaces.Repositories;
 using WayPoint.Application.Common.Mappings;
 using WayPoint.Application.Riders.DTOs;
 using WayPoint.Domain.Common;
-using WayPoint.Domain.Enums;
 
-namespace WayPoint.Application.Riders.Queries.GetAvailableRiders;
+namespace WayPoint.Application.Riders.Queries.GetRiders;
 
-public class GetAvailableRidersQueryHandler
-    : IRequestHandler<GetAvailableRidersQuery, Result<List<RiderDto>>>
+public class GetRidersQueryHandler
+    : IRequestHandler<GetRidersQuery, Result<List<RiderDto>>>
 {
     private readonly IRiderRepository _repo;
     private readonly RiderMapper _mapper;
 
-    public GetAvailableRidersQueryHandler(IRiderRepository repo, RiderMapper mapper)
+    public GetRidersQueryHandler(IRiderRepository repo, RiderMapper mapper)
     {
         _repo = repo;
         _mapper = mapper;
     }
 
     public async Task<Result<List<RiderDto>>> Handle(
-        GetAvailableRidersQuery request, CancellationToken ct)
+        GetRidersQuery request, CancellationToken ct)
     {
-        var riders = await _repo.GetByStatusAsync(RiderStatus.Available, ct);
+        if (request.Status is null)
+        {
+            return Result<List<RiderDto>>.Success(new List<RiderDto>());
+        }
+
+        var riders = await _repo.GetByStatusAsync(request.Status.Value, ct);
 
         var riderDtos = riders.Select(rider => _mapper.ToDto(rider)).ToList();
 
