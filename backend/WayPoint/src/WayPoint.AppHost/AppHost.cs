2359caf0655680e48ddb64ac090392790f97806a
Phiriyadet <54772372+Phiriyadet@users.noreply.github.com>
2026-03-19 14:29:20 +0700

    feat(apphost) : add code for use aspire podtgre and redis

--- backend/WayPoint/src/WayPoint.AppHost/AppHost.cs
+++ backend/WayPoint/src/WayPoint.AppHost/AppHost.cs
@@ -1,5 +1,19 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.WayPoint_WebApi>("waypoint-webapi");
+
+// 1. เพิ่ม PostgreSQL + PostGIS
+var postgres = builder.AddPostgres("DefaultConnection")
+    .WithImage("postgis/postgis", "16-3.4")
+    .WithPgAdmin()  // เปิด pgAdmin UI ที่ http://localhost:5050
+    .AddDatabase("waypoint");
+
+// 2. เพิ่ม Redis
+var redis = builder.AddRedis("Redis")
+    .WithRedisCommander();  // เปิด Redis UI ที่ http://localhost:8081
+
+// 3. เพิ่ม API project
+var api = builder.AddProject<Projects.WayPoint_WebApi>("waypoint-webapi")
+    .WithReference(postgres)
+    .WithReference(redis);
 
 builder.Build().Run();
